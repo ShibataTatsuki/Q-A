95d354c5c0da696c8e6c917d1588c1cf0acb6b52
柴田　樹希 <shibatatatsuki0805@gmail.com>
2017-07-22 19:58:17 +0900

    Initial Commit

---
+++ Q&A/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Q&A
+//
+//  Created by 柴田　樹希 on 2017/07/22.
+//  Copyright © 2017年 柴田　樹希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
